f001e25a39853b953ea0ee826ef307d319aec2fc
MouadHsb <152964260+MouadHsb@users.noreply.github.com>
2025-02-19 09:40:44 +0100

    backend

--- src/index.js
+++ src/index.js
@@ -42,5 +42,4 @@ app.post("/api/analyze-skin", (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+module.exports = app;
